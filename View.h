1faf5ae2767b01dd6c242da9b27adf89fce97bd2
pipinkurniawati <13513089@std.stei.itb.ac.id>
2016-02-20 22:27:12 +0700

    add view

---
+++ View.h
@@ -0,0 +1,18 @@
+#ifndef VIEW_H
+#define VIEW_H
+
+#include "Window.h"
+#include <vector>
+
+class View {
+	public:
+		Point P1; //top left
+		Point P2; //bottom right
+		vector<Line> lines;
+
+		View();
+		View(Point P1, Point P2);
+		void setViewLines(Window window);
+};
+
+#endif
